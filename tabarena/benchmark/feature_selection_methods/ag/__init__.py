c296c1ae439b1f5f01d2940cc6dc06a50ecd6f03
bastis007 <schaefer.bastian@mail.de>
2025-11-05 17:24:13 +0100

    Upload init files and AbstractFeatureGenerator implementation of Select_k_Best_Chi2 Feature Selection Method

---
+++ tabarena/benchmark/feature_selection_methods/ag/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from tabarena.benchmark.models.ag.ebm.ebm_model import ExplainableBoostingMachineModel
+from tabarena.benchmark.models.ag.modernnca.modernnca_model import ModernNCAModel
+from tabarena.benchmark.models.ag.realmlp.realmlp_model import RealMLPModel
+from tabarena.benchmark.models.ag.tabdpt.tabdpt_model import TabDPTModel
+from tabarena.benchmark.models.ag.tabicl.tabicl_model import TabICLModel
+from tabarena.benchmark.models.ag.tabm.tabm_model import TabMModel
+from tabarena.benchmark.models.ag.tabpfnv2.tabpfnv2_client_model import TabPFNV2ClientModel
+from tabarena.benchmark.models.ag.tabpfnv2.tabpfnv2_model import TabPFNV2Model
+from tabarena.benchmark.models.ag.xrfm.xrfm_model import XRFMModel
+from tabarena.benchmark.models.ag.knn_new.knn_model import KNNNewModel
+__all__ = [
+    "select_k_best_chi2",
+]
---
+++ tabarena/benchmark/feature_selection_methods/ag/select_k_best_chi2/select_k_best_chi2.py
@@ -0,0 +1,47 @@
+import logging
+
+from autogluon.common.features.types import R_INT, R_FLOAT, R_OBJECT
+from pandas import DataFrame, Series
+
+from autogluon.features.generators.abstract import AbstractFeatureGenerator
+
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+
+logger = logging.getLogger(__name__)
+
+
+class Select_k_Best_Chi2(AbstractFeatureGenerator):
+    """ Select k best features from the data using Chi^2 score """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._select_best = None
+        self._y = None
+
+
+    def _fit_transform(self, X: DataFrame, y: Series, **kwargs) -> tuple[DataFrame, dict]:
+        self._y = y
+
+        self._select_best_kwargs = {"score_func": chi2, "k": 3}
+        self._select_best = SelectKBest(**self._select_best_kwargs).set_output(transform="pandas")
+        X_out = self._transform(X, is_train=True)
+
+        selected_features = list(X_out.columns)
+        self.feature_metadata_in.keep_features(selected_features, inplace=True)
+        type_family_groups_special = {}
+
+        return X_out, type_family_groups_special
+
+
+    def _transform(self, X: DataFrame, *, is_train: bool = False) -> DataFrame:
+        if is_train:
+            X = self._select_best.fit_transform(X, self._y)
+        else:
+            X = self._select_best.transform(X)
+        return X
+
+
+    @staticmethod
+    def get_default_infer_features_in_args() -> dict:
+        return dict(valid_raw_types=[R_INT, R_FLOAT, R_OBJECT])
