42a18e6652d3ccda6d359e192cc3443b17f74ebd
bastis007 <schaefer.bastian@mail.de>
2025-12-19 11:24:49 +0100

    Infer problem type and fix bugs using self._selected_features instead of self.feature_metadata_in

--- tabarena/benchmark/feature_selection_methods/ag/mafese/MAFESE.py
+++ tabarena/benchmark/feature_selection_methods/ag/mafese/MAFESE.py
@@ -6,6 +6,7 @@ from mafese import UnsupervisedSelector
 from autogluon.common.features.types import R_INT, R_FLOAT, R_OBJECT
 from autogluon.features.generators.abstract import AbstractFeatureSelector
 
+
 class MAFESE(AbstractFeatureSelector):
     """ Select features from the data using Boruta algorithm """
 
@@ -13,10 +14,22 @@ class MAFESE(AbstractFeatureSelector):
         super().__init__(**kwargs)
         self._mafese = None
         self._y = None
+        self._model = None
+        self._n_max_features = None
         self._selected_features = None
 
-    def _fit_transform(self, X: DataFrame, y: Series, **kwargs) -> tuple[DataFrame, dict]:
+    def _fit_transform(self, X: DataFrame, y: Series, model, n_max_features, **kwargs) -> tuple[DataFrame, dict]:
         self._y = y
+        self._model = model
+        self._n_max_features = n_max_features
+
+        problem_type = self._infer_problem_type(y=y)
+        if problem_type == "binary":
+            problem = "classification"
+        elif problem_type == "multiclass":
+            problem = "classification"
+        else:
+            problem = "regression"
 
         data = Data(X, y)
         data.split_train_test(test_size=0.1, inplace=True)
@@ -26,26 +39,22 @@ class MAFESE(AbstractFeatureSelector):
         data.y_train, scaler_y = data.encode_label(data.y_train)  # This is for classification problem only
         data.y_test = scaler_y.transform(data.y_test)
 
-        self._mafese_kwargs = {"problem": "classification", "method": "DR", "n_features": 5}
+        self._mafese_kwargs = {"problem": problem, "method": "DR", "n_features": n_max_features}
         self._mafese = UnsupervisedSelector(**self._mafese_kwargs)
         self._mafese.fit(data.X_train, data.y_train)
         selected_features_indexes = self._mafese.selected_feature_indexes
         X_out = X.iloc[:, selected_features_indexes]
-        selected_features = X_out.columns
-
-        self.feature_metadata_in.keep_features(selected_features, inplace=True)
+        self._selected_features = list(X_out.columns)
         type_family_groups_special = {}
-
         return X_out, type_family_groups_special
 
     def _transform(self, X: DataFrame, *, is_train: bool = False) -> DataFrame:
         if is_train:
-            X = self._mafese.fit_transform(X, self._y)
+            X = self._mafese.fit_transform(X, self._y, self._model, self._n_max_features)
+            self._selected_features = list(X.columns)
         else:
-            selected_features_indexes = self._mafese.selected_feature_indexes
-            X = X.iloc[:, selected_features_indexes]
+            X = X[self._selected_features]
         return X
-
 
     @staticmethod
     def get_default_infer_features_in_args() -> dict:
