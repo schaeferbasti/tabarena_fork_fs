269dea9b12a0f51e82b6e4ecf6bb1bd4e3869567
bastis007 <schaefer.bastian@mail.de>
2026-01-21 17:46:40 +0100

    Implement ReliefF using unofficial implementation (https://github.com/jundongl/scikit-feature/blob/48cffad4e88ff4b9d2f1c7baffb314d1b3303792/skfeature/function/similarity_based/reliefF.py)

--- tabarena/benchmark/feature_selection_methods/ag/relieff/method/ReliefF.py
+++ tabarena/benchmark/feature_selection_methods/ag/relieff/method/ReliefF.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
+import numpy as np
 import pandas as pd
 
+from sklearn.metrics import pairwise_distances
+
 import warnings
-
-from sklearn.model_selection import train_test_split
-
-import copy
 import logging
-logger = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 warnings.filterwarnings('ignore')
 
 
@@ -22,14 +21,19 @@ class ReliefF:
         self._n_max_features = None
         self._selected_features = None
 
-
     def fit_transform(self, X: pd.DataFrame, y: pd.Series, model, n_max_features, **kwargs) -> pd.DataFrame:
         self._y = y
         self._model = model
         self._n_max_features = n_max_features
-        init_feature_choice = [0] * len(X.columns)
-        X_selected = self.relieff(X, y, model, n_max_features, init_feature_choice, **kwargs)
-        X_selected = pd.DataFrame(X, columns=X_selected.columns, index=X.index)
+        X_np = X.to_numpy()
+        y_np = y.to_numpy()
+
+        feature_ranking = self.feature_ranking(self.reliefF(X_np, y_np))
+
+        selected_features_idx = feature_ranking[:n_max_features]
+        selected_features = X.columns[selected_features_idx]
+
+        X_selected = X[selected_features]
         self._selected_features = list(X_selected.columns)
         return X_selected
 
@@ -40,15 +44,110 @@ class ReliefF:
         return X[self._selected_features]
 
 
-    def relieff(self, X_train, y_train, model, n_max_features, feature_indices, **kwargs):
-        return X_train
+    def reliefF(self, X, y, **kwargs):
+        """
+        This function implements the reliefF feature selection
 
+        Input
+        -----
+        X: {numpy array}, shape (n_samples, n_features)
+            input data
+        y: {numpy array}, shape (n_samples,)
+            input class labels
+        kwargs: {dictionary}
+            parameters of reliefF:
+            k: {int}
+                choices for the number of neighbors (default k = 5)
 
-    @staticmethod
-    def evaluate_subset(self, X, y, model):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        model_copy = copy.deepcopy(model)
-        model_copy.params["fold_fitting_strategy"] = "sequential_local"
-        model_copy = model_copy.fit(X=X_train, y=y_train, k_fold=8)
-        self._model = model_copy
-        return model_copy.score_with_oof(y=y_train)
+        Output
+        ------
+        score: {numpy array}, shape (n_features,)
+            reliefF score for each feature
+
+        Reference
+        ---------
+        Robnik-Sikonja, Marko et al. "Theoretical and empirical analysis of relieff and rrelieff." Machine Learning 2003.
+        Zhao, Zheng et al. "On Similarity Preserving Feature Selection." TKDE 2013.
+        """
+
+        if "k" not in kwargs.keys():
+            k = 5
+        else:
+            k = kwargs["k"]
+        n_samples, n_features = X.shape
+
+        # calculate pairwise distances between instances
+        distance = pairwise_distances(X, metric='manhattan')
+
+        score = np.zeros(n_features)
+
+        # the number of sampled instances is equal to the number of total instances
+        for idx in range(n_samples):
+            near_hit = []
+            near_miss = dict()
+
+            self_fea = X[idx, :]
+            c = np.unique(y).tolist()
+
+            stop_dict = dict()
+            for label in c:
+                stop_dict[label] = 0
+            del c[c.index(y[idx])]
+
+            p_dict = dict()
+            p_label_idx = float(len(y[y == y[idx]])) / float(n_samples)
+
+            for label in c:
+                p_label_c = float(len(y[y == label])) / float(n_samples)
+                p_dict[label] = p_label_c / (1 - p_label_idx)
+                near_miss[label] = []
+
+            distance_sort = []
+            distance[idx, idx] = np.max(distance[idx, :])
+
+            for i in range(n_samples):
+                distance_sort.append([distance[idx, i], int(i), y[i]])
+            distance_sort.sort(key=lambda x: x[0])
+
+            for i in range(n_samples):
+                # find k nearest hit points
+                if distance_sort[i][2] == y[idx]:
+                    if len(near_hit) < k:
+                        near_hit.append(distance_sort[i][1])
+                    elif len(near_hit) == k:
+                        stop_dict[y[idx]] = 1
+                else:
+                    # find k nearest miss points for each label
+                    if len(near_miss[distance_sort[i][2]]) < k:
+                        near_miss[distance_sort[i][2]].append(distance_sort[i][1])
+                    else:
+                        if len(near_miss[distance_sort[i][2]]) == k:
+                            stop_dict[distance_sort[i][2]] = 1
+                stop = True
+                for (key, value) in stop_dict.items():
+                    if value != 1:
+                        stop = False
+                if stop:
+                    break
+
+            # update reliefF score
+            near_hit_term = np.zeros(n_features)
+            for ele in near_hit:
+                near_hit_term = np.array(abs(self_fea - X[ele, :])) + np.array(near_hit_term)
+
+            near_miss_term = dict()
+            for (label, miss_list) in near_miss.items():
+                near_miss_term[label] = np.zeros(n_features)
+                for ele in miss_list:
+                    near_miss_term[label] = np.array(abs(self_fea - X[ele, :])) + np.array(near_miss_term[label])
+                score += near_miss_term[label] / (k * p_dict[label])
+            score -= near_hit_term / k
+        return score
+
+    def feature_ranking(self, score):
+        """
+        Rank features in descending order according to reliefF score, the higher the reliefF score, the more important the
+        feature is
+        """
+        idx = np.argsort(score, 0)
+        return idx[::-1]
