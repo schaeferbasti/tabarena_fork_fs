b19e1f087d921c554ec8bc21f7f6ea2e1c7d81b1
bastis007 <schaefer.bastian@mail.de>
2026-01-22 07:58:57 +0100

    Implement DISR using unofficial implementation (https://github.com/jundongl/scikit-feature/blob/48cffad4e88ff4b9d2f1c7baffb314d1b3303792/skfeature/function/information_theoretical_based/DISR.py)

--- tabarena/benchmark/feature_selection_methods/ag/disr/method/DISR.py
+++ tabarena/benchmark/feature_selection_methods/ag/disr/method/DISR.py
@@ -1,15 +1,14 @@
 from __future__ import annotations
 
+from math import log
+
+import numpy as np
 import pandas as pd
 
 import warnings
-
-from sklearn.model_selection import train_test_split
-
-import copy
 import logging
-logger = logging.getLogger(__name__)
 
+logger = logging.getLogger(__name__)
 warnings.filterwarnings('ignore')
 
 
@@ -22,33 +21,172 @@ class DISR:
         self._n_max_features = None
         self._selected_features = None
 
-
     def fit_transform(self, X: pd.DataFrame, y: pd.Series, model, n_max_features, **kwargs) -> pd.DataFrame:
         self._y = y
         self._model = model
         self._n_max_features = n_max_features
-        init_feature_choice = [0] * len(X.columns)
-        X_selected = self.disr(X, y, model, n_max_features, init_feature_choice, **kwargs)
-        X_selected = pd.DataFrame(X, columns=X_selected.columns, index=X.index)
+        X_np = X.to_numpy()
+        y_np = y.to_numpy()
+
+        feature_ranking = self.disr(X_np, y_np, n_selected_features=n_max_features)
+
+        selected_features_idx = feature_ranking[:n_max_features]
+        selected_features = X.columns[selected_features_idx]
+
+        X_selected = X[selected_features]
         self._selected_features = list(X_selected.columns)
         return X_selected
-
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         if self._selected_features is None:
             self.fit_transform(X, self._y, self._model, self._n_max_features)
         return X[self._selected_features]
 
+    def disr(self, X, y, **kwargs):
+        """
+        This function implement the DISR feature selection.
+        The scoring criteria is calculated based on the formula j_disr=sum_j(I(f,fj;y)/H(f,fj,y))
 
-    def disr(self, X_train, y_train, model, n_max_features, feature_indices, **kwargs):
-        return X_train
+        Input
+        -----
+        X: {numpy array}, shape (n_samples, n_features)
+            input data, guaranteed to be a discrete data matrix
+        y: {numpy array}, shape (n_samples,)
+            input class labels
+
+        kwargs: {dictionary}
+            n_selected_features: {int}
+                number of features to select
+
+        Output
+        ------
+        F: {numpy array}, shape (n_features, )
+            index of selected features, F[0] is the most important feature
+        J_DISR: {numpy array}, shape: (n_features,)
+            corresponding objective function value of selected features
+        MIfy: {numpy array}, shape: (n_features,)
+            corresponding mutual information between selected features and response
+
+        Reference
+        ---------
+        Brown, Gavin et al. "Conditional Likelihood Maximisation: A Unifying Framework for Information Theoretic Feature Selection." JMLR 2012.
+        """
+
+        n_samples, n_features = X.shape
+        # index of selected features, initialized to be empty
+        F = []
+        # Objective function value for selected features
+        J_DISR = []
+        # Mutual information between feature and response
+        MIfy = []
+        # indicate whether the user specifies the number of features
+        is_n_selected_features_specified = False
+
+        if 'n_selected_features' in kwargs.keys():
+            n_selected_features = kwargs['n_selected_features']
+            is_n_selected_features_specified = True
+
+        # sum stores sum_j(I(f,fj;y)/H(f,fj,y)) for each feature f
+        sum = np.zeros(n_features)
+
+        # make sure that j_cmi is positive at the very beginning
+        j_disr = 1
+
+        while True:
+            if len(F) == 0:
+                # t1 stores I(f;y) for each feature f
+                t1 = np.zeros(n_features)
+                for i in range(n_features):
+                    f = X[:, i]
+                    t1[i] = self.midd(f, y)
+                # select the feature whose mutual information is the largest
+                idx = np.argmax(t1)
+                F.append(idx)
+                J_DISR.append(t1[idx])
+                MIfy.append(t1[idx])
+                f_select = X[:, idx]
+
+            if is_n_selected_features_specified is True:
+                if len(F) == n_selected_features:
+                    break
+            if is_n_selected_features_specified is not True:
+                if j_disr <= 0:
+                    break
+
+            # we assign an extreme small value to j_disr to ensure that it is smaller than all possible value of j_disr
+            j_disr = -1E30
+            for i in range(n_features):
+                if i not in F:
+                    f = X[:, i]
+                    t2 = self.midd(f_select, y) + self.cmidd(f, y, f_select)
+                    t3 = self.entropyd(f) + self.conditional_entropy(f_select, f) + (
+                                self.conditional_entropy(y, f_select) - self.cmidd(y, f, f_select))
+                    sum[i] += np.true_divide(t2, t3)
+                    # record the largest j_disr and the corresponding feature index
+                    if sum[i] > j_disr:
+                        j_disr = sum[i]
+                        idx = i
+            F.append(idx)
+            J_DISR.append(j_disr)
+            MIfy.append(t1[idx])
+            f_select = X[:, idx]
+
+        return np.array(F)
 
 
-    @staticmethod
-    def evaluate_subset(self, X, y, model):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        model_copy = copy.deepcopy(model)
-        model_copy.params["fold_fitting_strategy"] = "sequential_local"
-        model_copy = model_copy.fit(X=X_train, y=y_train, k_fold=8)
-        self._model = model_copy
-        return model_copy.score_with_oof(y=y_train)
+    def conditional_entropy(self, f1, f2):
+        """
+        This function calculates the conditional entropy, where ce = H(f1) - I(f1;f2)
+
+        Input
+        -----
+        f1: {numpy array}, shape (n_samples,)
+        f2: {numpy array}, shape (n_samples,)
+
+        Output
+        ------
+        ce: {float}
+            ce is conditional entropy of f1 and f2
+        """
+
+        ce = self.entropyd(f1) - self.midd(f1, f2)
+        return ce
+
+    def midd(self, x, y):
+        """
+        Discrete mutual information estimator given a list of samples which can be any hashable object
+        """
+
+        return -self.entropyd(list(zip(x, y))) + self.entropyd(x) + self.entropyd(y)
+
+    def entropyd(self, sx, base=2):
+        """
+        Discrete entropy estimator given a list of samples which can be any hashable object
+        """
+
+        return self.entropyfromprobs(self.hist(sx), base=base)
+
+    def cmidd(self, x, y, z):
+        """
+        Discrete mutual information estimator given a list of samples which can be any hashable object
+        """
+
+        return self.entropyd(list(zip(y, z))) + self.entropyd(list(zip(x, z))) - self.entropyd(list(zip(x, y, z))) - self.entropyd(z)
+
+    def hist(self, sx):
+        # Histogram from list of samples
+        d = dict()
+        for s in sx:
+            d[s] = d.get(s, 0) + 1
+        return map(lambda z: float(z) / len(sx), d.values())
+
+    def entropyfromprobs(self, probs, base=2):
+        # Turn a normalized list of probabilities of discrete outcomes into entropy (base 2)
+        return -sum(map(self.elog, probs)) / log(base)
+
+    def elog(self, x):
+        # for entropy, 0 log 0 = 0. but we get an error for putting log 0
+        if x <= 0. or x >= 1.:
+            return 0
+        else:
+            return x * log(x)
