54fed0f40dbbd829cdce05b830a19ff0f2d9224a
bastis007 <schaefer.bastian@mail.de>
2026-01-22 14:11:44 +0100

    Implement Information Gain using unofficial implementation

--- tabarena/benchmark/feature_selection_methods/ag/information_gain/method/InformationGain.py
+++ tabarena/benchmark/feature_selection_methods/ag/information_gain/method/InformationGain.py
@@ -1,15 +1,19 @@
 from __future__ import annotations
 
+import math
+from collections import Counter
+
 import numpy as np
 import pandas as pd
 
 import warnings
 
-from sklearn.model_selection import train_test_split
+from scipy.stats import entropy
 
 import copy
 import logging
 import time
+
 logger = logging.getLogger(__name__)
 
 warnings.filterwarnings('ignore')
@@ -24,33 +28,133 @@ class InformationGain:
         self._n_max_features = None
         self._selected_features = None
 
-
-    def fit_transform(self, X: pd.DataFrame, y: pd.Series, model, n_max_features, **kwargs) -> pd.DataFrame:
+    def fit_transform(self, X: pd.DataFrame, y: pd.Series, model=None, n_max_features=None, **kwargs) -> pd.DataFrame:
         self._y = y
         self._model = model
         self._n_max_features = n_max_features
-        init_feature_choice = [0] * len(X.columns)
-        X_selected = self.information_gain(X, y, model, n_max_features, init_feature_choice, **kwargs)
-        X_selected = pd.DataFrame(X, columns=X_selected.columns, index=X.index)
+
+        igr_scores = []
+        for col in X.columns:
+            igr = self._information_gain_ratio(X[[col]], col, y)
+            igr_scores.append(igr)
+
+        igr_scores = np.array(igr_scores)
+        top_features_idx = np.argsort(igr_scores)[-n_max_features:][::-1]
+        X_selected = X.iloc[:, top_features_idx]
         self._selected_features = list(X_selected.columns)
         return X_selected
-
 
     def transform(self, X: pd.DataFrame) -> pd.DataFrame:
         if self._selected_features is None:
             self.fit_transform(X, self._y, self._model, self._n_max_features)
         return X[self._selected_features]
 
-
-    def information_gain(self, X_train, y_train, model, n_max_features, feature_indices, **kwargs):
-        return X_train
-
-
     @staticmethod
-    def evaluate_subset(self, X, y, model):
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-        model_copy = copy.deepcopy(model)
-        model_copy.params["fold_fitting_strategy"] = "sequential_local"
-        model_copy = model_copy.fit(X=X_train, y=y_train, k_fold=8)
-        self._model = model_copy
-        return model_copy.score_with_oof(y=y_train)
+    def _entropy(values):
+        """
+        Calculate entropy from a frequency distribution.
+
+        Parameters
+        ----------
+        values : array-like
+            Value counts or frequencies
+
+        Returns
+        -------
+        float
+            Entropy value
+        """
+        # Normalize to probabilities and compute entropy
+        probabilities = values / values.sum()
+        return entropy(probabilities, base=2)
+
+    def _information_gain(self, X: pd.DataFrame, feature: str, y: pd.Series) -> float:
+        """
+        Compute information gain for a single feature.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Feature DataFrame
+        feature : str
+            Column name of the feature
+        y : pd.Series
+            Target variable
+
+        Returns
+        -------
+        float
+            Information gain value
+        """
+        # Calculate entropy of the target variable
+        H_y = self._entropy(y.value_counts())
+
+        # Calculate weighted conditional entropy
+        weighted_entropy = 0.0
+
+        for value in X[feature].unique():
+            # Get mask for rows where feature equals this value
+            mask = X[feature] == value
+
+            # Get the subset of target values
+            y_subset = y[mask]
+
+            if len(y_subset) > 0:
+                # Weight by proportion of this value
+                weight = len(y_subset) / len(y)
+
+                # Calculate entropy of this subset
+                subset_entropy = self._entropy(y_subset.value_counts())
+
+                # Add weighted entropy
+                weighted_entropy += weight * subset_entropy
+
+        # Information gain = H(Y) - H(Y|feature)
+        return H_y - weighted_entropy
+
+    def _intrinsic_value(self, X: pd.DataFrame, feature: str) -> float:
+        """
+        Compute intrinsic value (split information) for a feature.
+        Used for information gain ratio normalization.
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Feature DataFrame
+        feature : str
+            Column name of the feature
+
+        Returns
+        -------
+        float
+            Intrinsic value
+        """
+        value_counts = X[feature].value_counts()
+        proportions = value_counts / len(X)
+        return entropy(proportions, base=2)
+
+    def _information_gain_ratio(self, X: pd.DataFrame, feature: str, y: pd.Series) -> float:
+        """
+        Compute information gain ratio (normalized by intrinsic value).
+
+        Parameters
+        ----------
+        X : pd.DataFrame
+            Feature DataFrame
+        feature : str
+            Column name of the feature
+        y : pd.Series
+            Target variable
+
+        Returns
+        -------
+        float
+            Information gain ratio
+        """
+        ig = self._information_gain(X, feature, y)
+        iv = self._intrinsic_value(X, feature)
+
+        # Avoid division by zero
+        if iv == 0:
+            return 0.0
+        return ig / iv
