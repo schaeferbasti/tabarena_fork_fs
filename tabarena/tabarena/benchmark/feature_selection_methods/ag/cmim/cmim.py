e220166f8e9301503c8257a092714a66231a2e84
domiomi3 <matusd@informatik.uni-freiburg.de>
2026-04-27 13:46:42 +0200

    fix: refactor cmim and consistency

--- tabarena/tabarena/benchmark/feature_selection_methods/ag/cmim/cmim.py
+++ tabarena/tabarena/benchmark/feature_selection_methods/ag/cmim/cmim.py
@@ -18,10 +18,9 @@ class CMIMFeatureSelector(AbstractITFeatureSelector):
     Journal of Machine learning research 5.Nov (2004): 1531-1555.
     Implementation Source: https://github.com/jundongl/scikit-feature/blob/48cffad4e88ff4b9d2f1c7baffb314d1b3303792/skfeature/function/information_theoretical_based/CMIM.py#L4.
     The author of the code is Li, Jundong, Associate Professor at the University of Virginia and main-author of 'Feature selection: A data perspective' (2017).
-    Changes to the implementation by Bastian Schäfer:
-                           - Add time constraint
-                           - Add max_features (number of features to be maximally selected by the method) constraint
-                           - Use pandas instead of numpy and avoid conversion
+    Changes to the implementation:
+        - Time and max_features constraint
+        - Use pandas instead of numpy and avoid conversion
     """
 
     name = "CMIMFeatureSelector"
@@ -31,42 +30,43 @@ class CMIMFeatureSelector(AbstractITFeatureSelector):
         self, *, X: pd.DataFrame, y: pd.Series, time_limit: int | None = None
     ) -> list[str]:
         start_time = time.monotonic()
-        n_features = len(X.columns)
-
         X_pre, _ = self._preprocess(X, impute=True, discretize=True, encode_ordinal=True)
+        n_features = len(X_pre.columns)
         cols = [X_pre.iloc[:, i] for i in range(n_features)]
 
-        CMIM = np.array([self._mutual_information(col, y) for col in cols])
-        m = -np.ones(n_features, dtype=int)
-        F: list[int] = []
+        partial_scores = np.array([self._mutual_information(col, y) for col in cols])
+        m = -np.ones(n_features, dtype=int) 
+        selected_idx: list[int] = [] # selected features indices
+        selected_mask = np.zeros(n_features, dtype=bool)
 
-        for k in range(min(n_features, self.max_features)): # includes early stopping
+        # Fleuret's algorithm
+        for k in range(min(n_features, self.max_features)): # stop when max_features reached
             if self._timed_out(time_limit, start_time):
                 break
 
-            # Choose the feature with the highest MI as the next feature
-            idx = np.argmax(CMIM)
-            F.append(idx)
-            CMIM[idx] = -np.inf
-            if len(F) == self.max_features:
+            idx = int(np.argmax(partial_scores))  # next feature with the highest cmi
+            selected_idx.append(idx)
+            selected_mask[idx] = True
+            partial_scores[idx] = -np.inf
+            
+            if len(selected_idx) == self.max_features:
                 break
 
-            sstar = -np.inf # start with really low value for best partial score sstar
+            sstar = -np.inf # keeps lowest mi score 
             for i in range(n_features):
-                if self._timed_out(time_limit, start_time):
-                    break
-                if i in F:
+                if selected_mask[i]:
                     continue
-                while (CMIM[i] > sstar) and (m[i] < k):
+                while partial_scores[i] > sstar and m[i] < k:
                     if self._timed_out(time_limit, start_time):
                         break
                     m[i] += 1
-                    CMIM[i] = min(CMIM[i], self._conditional_mutual_information(cols[i], y, cols[int(F[int(m[i])])]))
-                if CMIM[i] > sstar:
-                    sstar = CMIM[i]
+                    partial_scores[i] = min(
+                        partial_scores[i], 
+                        self._conditional_mutual_information(cols[i], y, cols[selected_idx[m[i]]])
+                    )
+                if partial_scores[i] > sstar:
+                    sstar = partial_scores[i]
         
-        selected_features = [self._original_features[i] for i in F]
-        if len(selected_features) < self.max_features:
-            selected_features += self.fallback_feature_selection(selected_features=selected_features)
+        selected_features = [self._original_features[i] for i in selected_idx]
         return [str(feat) for feat in selected_features]
 
